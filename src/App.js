15428bcb4c2dbde3da60780f13afce5b96bd5618
Maharshi17Barot <99244382+Maharshi17Barot@users.noreply.github.com>
2023-11-12 06:42:09 -0500

    Week 09 commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./components/Welcome";
+import Student from "./components/Student";
+import College from "./components/College";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome />
+        <Student sid="101380593" sname="Maharshi barot" />
+        <College />
       </header>
     </div>
   );
